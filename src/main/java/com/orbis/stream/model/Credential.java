9d89be11226457b193117f231b90fb0218c33243
zaccalos7 <guarin719@outlook.it>
2026-03-02 23:47:31 +0100

    added model

---
+++ src/main/java/com/orbis/stream/model/Credential.java
@@ -0,0 +1,23 @@
+package com.orbis.stream.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "credentials")
+public class Credential {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer pkid;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false, columnDefinition = "BOOLEAN DEFAULT FALSE")
+    private Boolean hasVerifiedEmail=false;
+}
---
+++ src/main/java/com/orbis/stream/model/User.java
@@ -0,0 +1,25 @@
+package com.orbis.stream.model;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer pkid;
+
+    @Column(nullable = false)
+    private String nickName;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private Credential credential;
+}
+
