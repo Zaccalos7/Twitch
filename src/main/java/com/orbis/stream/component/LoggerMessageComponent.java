6c80c5c704510d4c5ad6091fdf87e0bbfa37be04
zaccalos7 <guarin719@outlook.it>
2026-03-03 00:02:06 +0100

    added handled messages

---
+++ src/main/java/com/orbis/stream/component/LoggerMessageComponent.java
@@ -0,0 +1,21 @@
+package com.orbis.stream.component;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class LoggerMessageComponent {
+    private final MessageSource messageSource;
+
+    public String printMessage(String code){
+        return messageSource.getMessage(code, null, LocaleContextHolder.getLocale());
+    }
+
+
+    public String printMessage(String code, Object[] params){
+        return messageSource.getMessage(code , params, LocaleContextHolder.getLocale());
+    }
+}
