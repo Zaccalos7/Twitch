a24c57bb330943a4b3bbe2554d4aba855560ce63
zaccalos7 <guarin719@outlook.it>
2026-03-03 00:01:46 +0100

    added security

---
+++ src/main/java/com/orbis/stream/configuration/SecurityConfigurations.java
@@ -0,0 +1,48 @@
+package com.orbis.stream.configuration;
+
+import com.orbis.stream.component.LoggerMessageComponent;
+import com.orbis.stream.exceptions.FileReadingException;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+@Slf4j
+@Configuration
+public class SecurityConfigurations {
+
+
+    private final LoggerMessageComponent loggerMessageComponent;
+
+    public SecurityConfigurations( LoggerMessageComponent loggerMessageComponent) {
+        this.loggerMessageComponent = loggerMessageComponent;
+    }
+
+    public String retrievesConfigurations(){
+        String configurations;
+        configurations = getSignKey();
+        return configurations;
+    }
+
+    private String getSignKey(){
+        String signKey;
+        Path envPath = Path.of(".env").toAbsolutePath();
+
+        try(Stream<String> lines = Files.lines(envPath)){
+            signKey = lines
+                    .findFirst()
+                    .map(line-> line.split("=", 2)[1])
+                    .orElseThrow(()-> new FileReadingException("file.is.empty"));
+
+            return  signKey;
+        } catch (IOException e) {
+            log.error(loggerMessageComponent.printMessage("error.during.reading.key"));
+            log.error(e.getMessage(), e);
+            throw new FileReadingException("error.during.reading.key");
+        }
+    }
+}
---
+++ src/main/java/com/orbis/stream/security/JwtUtilComponent.java
@@ -0,0 +1,60 @@
+package com.orbis.stream.security;
+
+import com.orbis.stream.configuration.SecurityConfigurations;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.util.Date;
+
+@Component
+public class JwtUtilComponent {
+
+
+    private final SecurityConfigurations securityConfigurations;
+
+
+    public JwtUtilComponent(SecurityConfigurations securityConfigurations) {
+        this.securityConfigurations = securityConfigurations;
+    }
+
+    protected String generateToken(String subject) {
+
+        String token;
+        String secretKey = getValueFromEnv();
+
+        SecretKey secretKeyHmac = Keys.hmacShaKeyFor(
+                secretKey.getBytes(StandardCharsets.UTF_8)
+        );
+
+        long timestampMillis = System.currentTimeMillis();
+        long expirationDateMillis = getExpiration(timestampMillis);
+
+        token = Jwts.builder()
+                .setSubject(subject)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(expirationDateMillis))
+                .signWith(secretKeyHmac, SignatureAlgorithm.HS256)
+                .compact();
+
+        return token;
+    }
+
+    private String getValueFromEnv() {
+        return securityConfigurations.retrievesConfigurations();
+    }
+
+    private long getExpiration(long timestampMillis){
+
+        return Instant.ofEpochMilli(timestampMillis)
+                .atZone(ZoneId.systemDefault())
+                .plusMonths(1)
+                .toInstant()
+                .toEpochMilli();
+    }
+}
