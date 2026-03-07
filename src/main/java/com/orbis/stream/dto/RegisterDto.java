777f05672b6168f9568f4d9b41d26a0d0974e51a
zaccalos7 <guarin719@outlook.it>
2026-03-07 13:10:26 +0100

    added DTO for RegisterDto

---
+++ src/main/java/com/orbis/stream/dto/RegisterDto.java
@@ -0,0 +1,16 @@
+package com.orbis.stream.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class RegisterDto {
+    private String streamUrl;
+
+    private String streamKey;
+
+    private String platformStreamName;
+}
