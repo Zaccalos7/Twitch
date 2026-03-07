901d3ed24c240b9745c99bb7205d52910a7dd702
zaccalos7 <guarin719@outlook.it>
2026-03-07 22:26:50 +0100

    added createtion of setting configuration

--- src/main/java/com/orbis/stream/service/SettingService.java
+++ src/main/java/com/orbis/stream/service/SettingService.java
@@ -1,7 +1,61 @@
 package com.orbis.stream.service;
 
+import com.orbis.stream.component.LoggerMessageComponent;
+import com.orbis.stream.dto.SettingDto;
+import com.orbis.stream.handler.ResponseHandler;
+import com.orbis.stream.mapping.SettingMapper;
+import com.orbis.stream.model.Setting;
+import com.orbis.stream.model.User;
+import com.orbis.stream.repository.SettingRepository;
+import com.orbis.stream.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.transaction.annotation.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class SettingService {
+
+    private final LoggerMessageComponent loggerMessageComponent;
+    private final ResponseHandler responseHandler;
+
+    private final SettingRepository settingRepository;
+    private final UserRepository userRepository;
+
+    private final SettingMapper settingMapper;
+
+    //for now user is only mario
+    private static final String nickName = "Mario";
+
+    public ResponseEntity<Map<String, String>> addNewConfiguration(SettingDto settingDto){
+        ResponseEntity<Map<String, String>>  mapResponseEntity = responseHandler
+                .buildResponse("success.operations", HttpStatus.CREATED);
+
+        saveSettings(settingDto);
+
+        return mapResponseEntity;
+    }
+
+    @Transactional
+    private void saveSettings(SettingDto settingDto){
+        Setting setting = settingMapper.toModel(settingDto);
+        User user = userRepository.findByNickName(nickName);
+
+        //@TODO insert login and pass the real user
+        if(user == null){
+            log.error(loggerMessageComponent.printMessage("Utente Mario non trovato"));
+            throw new EntityNotFoundException("Utente Mario non trovato");
+        }
+
+        setting.setUser(user);
+        settingRepository.save(setting);
+
+    }
 }
