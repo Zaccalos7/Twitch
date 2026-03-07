02ad32c59b135bace01cdd5a22537d9f027a5f81
zaccalos7 <guarin719@outlook.it>
2026-03-07 15:18:51 +0100

    added check on nickname

--- src/main/java/com/orbis/stream/model/User.java
+++ src/main/java/com/orbis/stream/model/User.java
@@ -16,7 +16,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer pkid;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String nickName;
 
     @OneToOne(cascade = CascadeType.ALL)
