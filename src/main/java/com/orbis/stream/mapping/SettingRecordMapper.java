3f65f73602c0f226b86c83910b0e28b313d6ee8d
zaccalos7 <guarin719@outlook.it>
2026-03-08 16:12:58 +0100

    added mapper for update setting

--- src/main/java/com/orbis/stream/mapping/SettingRecordMapper.java
+++ src/main/java/com/orbis/stream/mapping/SettingRecordMapper.java
@@ -2,8 +2,15 @@ package com.orbis.stream.mapping;
 
 import com.orbis.stream.model.Setting;
 import com.orbis.stream.record.SettingRecord;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(componentModel = "spring", uses = {UserMapper.class})
 public interface SettingRecordMapper extends BaseMapper<SettingRecord, Setting>{
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateSettingFromSettingRecord(SettingRecord record, @MappingTarget Setting setting);
+
 }
