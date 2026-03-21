2d3dc6487a1eaea1d888499aac9720823197ab5f
zaccalos7 <guarin719@outlook.it>
2026-03-21 19:37:13 +0100

    added dummy start live of video

--- src/main/java/com/orbis/stream/controller/StreamController.java
+++ src/main/java/com/orbis/stream/controller/StreamController.java
@@ -1,15 +1,15 @@
 package com.orbis.stream.controller;
 
 import com.orbis.stream.component.LoggerMessageComponent;
+import com.orbis.stream.record.StartLiveRecord;
 import com.orbis.stream.service.StreamService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.bytedeco.javacv.FFmpegFrameGrabber;
-import org.bytedeco.javacv.FFmpegFrameRecorder;
-import org.bytedeco.javacv.FrameGrabber;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,7 +27,9 @@ public class StreamController {
     private final StreamService streamService;
 
     @GetMapping("/start")
-    public ResponseEntity<Map<String,String>> startLive() {
+    @Operation(summary = "Endpoint per lo start della live",
+            description = "Restituisce l'esito dell'operazione")
+    public ResponseEntity<Map<String,String>> startLive(@RequestBody StartLiveRecord startLiveRecord) {
         var response = streamService.startLive("/Users/zaccalos/Movies/input.mp4","live_1421368812_NOqfzzhID2sDFcdGgUwDOOmY7GOldJ");
         log.info("successo");
         return response;
