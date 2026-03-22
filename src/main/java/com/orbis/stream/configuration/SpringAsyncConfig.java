8c8cf97672ff99a399422d84b1cffd392a549f8c
zaccalos7 <guarin719@outlook.it>
2026-03-22 16:26:51 +0100

    added configuration for multi thread

---
+++ src/main/java/com/orbis/stream/configuration/SpringAsyncConfig.java
@@ -0,0 +1,24 @@
+package com.orbis.stream.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+
+@Configuration
+@EnableAsync
+public class SpringAsyncConfig {
+
+    @Bean(name = "taskExecutor")
+    public TaskExecutor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(3);
+        executor.setMaxPoolSize(6);
+        executor.setQueueCapacity(20);
+        executor.setThreadNamePrefix("Stream-Async-");
+        executor.initialize();
+        return executor;
+    }
+}
