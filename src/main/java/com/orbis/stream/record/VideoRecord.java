9c3b11ca861980de00502ebe7169ef1cfd6ef177
zaccalos7 <guarin719@outlook.it>
2026-04-27 20:48:13 +0200

    added  new records for video

---
+++ src/main/java/com/orbis/stream/record/VideoRecord.java
@@ -0,0 +1,21 @@
+package com.orbis.stream.record;
+
+import com.orbis.stream.enums.LiveStatusEnum;
+import lombok.Builder;
+
+
+@Builder
+public record VideoRecord( Integer pkid,
+
+         String name,
+
+         String videoPath,
+
+         String extension,
+
+         LiveStatusEnum liveStatus,
+
+         Long lastTimeStampBeforeStop,
+
+         String message) {
+}
