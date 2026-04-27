fbe7397a1d99a3c2a2f95d9b9af955e03d72fe56
zaccalos7 <guarin719@outlook.it>
2026-04-27 23:36:07 +0200

    modified fields for mapping other fields

--- src/main/java/com/orbis/stream/mapping/mapperDTO/VideoMapper.java
+++ src/main/java/com/orbis/stream/mapping/mapperDTO/VideoMapper.java
@@ -2,9 +2,10 @@ package com.orbis.stream.mapping.mapperDTO;
 
 import com.orbis.stream.dto.VideoDto;
 import com.orbis.stream.mapping.BaseMapper;
+import com.orbis.stream.mapping.mapperRECORD.VideoRecordMapper;
 import com.orbis.stream.model.Video;
 import org.mapstruct.Mapper;
 
-@Mapper(componentModel = "spring", uses = {VideoLiveHistoryMapper.class, V})
+@Mapper(componentModel = "spring", uses = {VideoLiveHistoryMapper.class, VideoRecordMapper.class})
 public interface VideoMapper extends BaseMapper<VideoDto, Video> {
 }
--- src/main/java/com/orbis/stream/record/VideoRecord.java
+++ src/main/java/com/orbis/stream/record/VideoRecord.java
@@ -3,19 +3,16 @@ package com.orbis.stream.record;
 import com.orbis.stream.enums.LiveStatusEnum;
 import lombok.Builder;
 
-
 @Builder
-public record VideoRecord( Integer pkid,
-
-         String name,
-
-         String videoPath,
-
-         String extension,
-
-         LiveStatusEnum liveStatus,
-
-         Long lastTimeStampBeforeStop,
-
-         String message) {
+public record VideoRecord(
+        Integer pkid,
+        String name,
+        String videoPath,
+        String extension,
+        LiveStatusEnum liveStatus,
+        Long lastTimeStampBeforeStop,
+        String message,
+        VideoLiveHistoryRecord videoLiveHistory,
+        VideoSettingsRecord videoSetting
+) {
 }
